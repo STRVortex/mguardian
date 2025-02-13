c0f0dd216747ebc223ae5342e1ddc9164bbda963
STRVortex <vortex8934@gmail.com>
2025-02-13 23:13:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,21 @@
-import os
+import re
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
 
 # Bot configuration
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # Replace with your bot token
-API_ID = int(os.environ.get("API_ID", ""))  # Replace with your API ID
-API_HASH = os.environ.get("API_HASH", "")  # Replace with your API hash
+BOT_TOKEN = getenv("BOT_TOKEN", "")  # Replace with your bot token
+API_ID = int(getenv("API_ID", ""))  # Replace with your API ID
+API_HASH = getenv("API_HASH", "")  # Replace with your API hash
 
 # Spoiler Mode configuration (set to True or False)
 SPOILER_MODE = os.environ.get("SPOILER_MODE", "True").lower() == "true"
 
 # MongoDB URI (default value if not set in environment)
-MONGO_URI = os.environ.get("MONGO_URI", "")  # MongoDB URI for your database
+MONGO_URI = getenv("MONGO_URI", "")  # MongoDB URI for your database
 
 # List of Sudo users (IDs of users with admin privileges)
 SUDO_USERS = [6257927828]  # Add the user ID(s) of the admins
